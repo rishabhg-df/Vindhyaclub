5cf62e9712a033ffec31449a274a0ea814bd9146
rish <rish3325@gmail.com>
2025-08-13 12:24:12 +0000

    facilities hero page image put

--- src/app/facilities/page.tsx
+++ src/app/facilities/page.tsx
@@ -5,8 +5,8 @@ export default function FacilitiesPage() {
   return (
     <>
       <Hero
-        imageUrl="https://images.unsplash.com/photo-1596423403483-a7e4a119514e?q=80&w=1920&auto=format&fit=crop"
-        imageHint="sports complex"
+        imageUrl="https://images.unsplash.com/photo-1579952363873-27f3bade9f55?q=80&w=1920&auto=format=fit=crop"
+        imageHint="sports facilities"
         title=""
         subtitle=""
         height="h-[70vh]"
