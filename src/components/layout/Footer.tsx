974d6b32ee9866fc02767c2aca19127d0f74bfa8
rish <rish3325@gmail.com>
2025-08-20 09:10:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -34,7 +34,7 @@ export function Footer() {
     { href: '/contact', label: 'Contact' },
   ];
 
-  const mapsUrl = 'https://maps.app.goo.gl/HLvSxaQ8k4jsd69u5';
+  const mapsUrl = 'https://maps.app.goo.gl/5sM3whx8RNETGMHV8';
   const embedUrl = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3628.291342993335!2d80.8351530149984!3d24.57947798418847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39845a246f90bd21%3A0x6b8f6789a71a5665!2sVindhya%20Club!5e0!3m2!1sen!2sin!4v1628087948251!5m2!1sen!2sin`;
 
   const contactDetails = [
